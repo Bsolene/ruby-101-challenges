f6ebefa94fa77c5f6f6d848aca75e4e30feda590
solene basaia <bsolene@hotmail.fr>
2017-04-28 17:29:01 +0100

    finish

--- 08-Palindrome-sentence/lib/better_palindrome.rb
+++ 08-Palindrome-sentence/lib/better_palindrome.rb
@@ -1,4 +1,10 @@
 def better_palindrome?(sentence)
   # TODO: return true/false depending if the *sentence* is a palindrome
-
+  sentence.downcase!
+  sentence = sentence.delete(" ")
+  sentence.gsub!(/[^0-9A-Za-z]/, '')
+  array_1 = sentence.split('')
+  array_2 = sentence.split('')
+  array_2 = array_1.reverse
+  array_1 == array_2
 end
