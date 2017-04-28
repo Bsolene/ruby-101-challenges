a5b56280e8cea78894289ccd3b08086c29f0a780
solene basaia <bsolene@hotmail.fr>
2017-04-28 17:22:34 +0100

    finish

--- 06-FizzBuzz/lib/fizzbuzz.rb
+++ 06-FizzBuzz/lib/fizzbuzz.rb
@@ -1,4 +1,16 @@
 def fizzbuzz(max)
   # TODO: return an Array
-
+  array = (1..max).to_a
+  final = array.map do |number|
+    if (number % 3).zero? && (number % 5).zero?
+      "FizzBuzz"
+    elsif (number % 3).zero?
+      "Fizz"
+    elsif (number % 5).zero?
+      "Buzz"
+    else
+      number
+    end
+  end
+  final
 end
