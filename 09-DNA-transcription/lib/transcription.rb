739967686807a2378f6a605f978711d81627b277
solene basaia <bsolene@hotmail.fr>
2017-04-28 18:07:15 +0100

    finish

--- 09-DNA-transcription/lib/transcription.rb
+++ 09-DNA-transcription/lib/transcription.rb
@@ -1,4 +1,20 @@
 def dna_transcription(dna_sequence)
   # TODO: return the RNA complement of the incoming DNA sequence
 
+  dna_string = dna_sequence.join('')
+  dna_string.upcase!
+  dna_string.gsub!(/[^GCATU]/,'')
+  dna_sequence = dna_string.split('')
+  rna = dna_sequence.map do |dna|
+    if dna == "G"
+      "C"
+    elsif dna == "C"
+      "G"
+    elsif dna == "T"
+      "A"
+    elsif dna == "A"
+      "U"
+    end
+  end
+  rna
 end
