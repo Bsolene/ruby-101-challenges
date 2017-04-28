8de930d18efe9137baf46d733b00540211471830
solene basaia <bsolene@hotmail.fr>
2017-04-28 17:07:01 +0100

    finsih

--- 03-Palindrome-word/lib/palindrome.rb
+++ 03-Palindrome-word/lib/palindrome.rb
@@ -1,4 +1,8 @@
 def palindrome?(word)
   # TODO: return true/false depending if the *word* is a palindrome
-
+  word.downcase!
+  array_1 = word.split('')
+  array_2 = word.split('').reverse!
+  array_1 == array_2
 end
+
