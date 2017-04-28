d54115d66956f59308bfca756c3fcaeeaf882c12
solene basaia <bsolene@hotmail.fr>
2017-04-28 17:14:47 +0100

    finish

--- 05-Pythagorean-triples/lib/pythagorean_triple.rb
+++ 05-Pythagorean-triples/lib/pythagorean_triple.rb
@@ -1,4 +1,12 @@
 def pythagorean_triple?(a, b, c)
   # TODO: return true/false depending if numbers a, b and c are Pythagorean triples
+  if (a.class || b.class || c.class) != Fixnum
+    false
+  else
+    a *= a
+    b *= b
+    c *= c
+    a + b == c
+  end
 
 end
